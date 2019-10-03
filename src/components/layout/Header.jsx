a6f1d913ae68fce854add73731cfb35401734623
Wei Li <Will@Newton-iMac.local>
2019-10-03 11:30:34 +1000

    initial project

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const Header = props => {
+    const {branding} = props;
+
+    return (
+        <div></div>
+    )
+}
