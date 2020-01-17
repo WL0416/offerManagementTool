39ba12796a9609ef4742f1366e54bd5184f159dd
Wei Li <Will@Newton-iMac.local>
2020-01-17 16:19:05 +1100

    update new offer page

--- src/components/pages/NewOffer.jsx
+++ src/components/pages/NewOffer.jsx
@@ -6,7 +6,18 @@ import axios from "axios";
 class NewOffer extends Component {
   state = {
     // the list of courses which is used to dynamicly add
-    courseList: [{ course: "", intake: "", duration: "" }]
+    courseList: {
+      couse0: { name: "", intake: "", duration: "" }
+    },
+    first_name: "",
+    last_name: "",
+    birthday: "",
+    passport: "",
+    phone: "",
+    email: "",
+    address: "",
+    enrolfee: "",
+    materialfee: ""
   };
 
   componentDidMount() {
@@ -33,7 +44,30 @@ class NewOffer extends Component {
     }
   };
 
-  handleSubmit = () => {};
+  handleSubmit = event => {
+    alert(
+      "data was submitted " +
+        this.state.first_name +
+        " " +
+        this.state.last_name +
+        " " +
+        this.state.email +
+        " " +
+        // this.state.courseList[0]["course0"] +
+        // " " +
+        this.state.enrolfee
+    );
+    event.preventDefault();
+  };
+
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
 
   render() {
     const { college, allcourses } = this.props;
@@ -49,46 +83,87 @@ class NewOffer extends Component {
         <br />
         <h1>New Offer ({college})</h1>
         <br />
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Row>
             <Form.Group as={Col} controlId="formGridFirstname">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="First Name" />
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                name="first_name"
+                value={this.state.first_name}
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridLastname">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Last Name" />
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                name="last_name"
+                value={this.state.last_name}
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridBirthday">
               <Form.Label>Date of Birth</Form.Label>
-              <Form.Control type="date" placeholder="Birthday" />
+              <Form.Control
+                type="date"
+                placeholder="Birthday"
+                name="birthday"
+                value={this.state.birthday}
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassport">
               <Form.Label>Passport No.</Form.Label>
-              <Form.Control type="text" placeholder="Passport Number" />
+              <Form.Control
+                type="text"
+                placeholder="Passport Number"
+                name="passport"
+                value={this.state.passport}
+                onChnage={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridPhone">
               <Form.Label>Phone No.</Form.Label>
-              <Form.Control type="number" placeholder="Phone Number" />
+              <Form.Control
+                type="number"
+                placeholder="Phone Number"
+                name="phone"
+                value={this.state.passport}
+                onChnage={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>E-mail</Form.Label>
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="formGridAddress">
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="Type in address here" />
+            <Form.Control
+              placeholder="Type in address here"
+              name="address"
+              value={this.state.address}
+              onChange={this.handleChange}
+            />
           </Form.Group>
 
           {/* add courses or delete courses */}
@@ -109,61 +184,74 @@ class NewOffer extends Component {
           </Button>
 
           {courseList.map((val, idx) => {
+            let courseId = `course${idx}`,
+              intakeId = `intake${idx}`,
+              durationId = `duration${idx}`;
             return (
               <Form.Row>
                 <Form.Group as={Col} controlId="formGridCourse">
                   <Form.Label>Course</Form.Label>
-                  <Form.Control as="select">
+                  <Form.Control
+                    as="select"
+                    name={courseId}
+                    onChange={this.handleChange}
+                    value={this.state.courseList[idx][courseId]}
+                  >
                     {allcourses.map(c => (
-                      <option>{c}</option>
+                      <option value={c}>{c}</option>
                     ))}
                   </Form.Control>
                 </Form.Group>
 
+                <p>{courseId}</p>
+                <p>{val[intakeId]}</p>
+
                 <Form.Group as={Col} controlId="formGridSart">
                   <Form.Label>Intake Date</Form.Label>
-                  <Form.Control type="date" placeholder="Birthday" />
+                  <Form.Control
+                    type="date"
+                    placeholder="Birthday"
+                    name={intakeId}
+                    value={this.state.courseList[idx][intakeId]}
+                    onChange={this.handleChange}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridDuration">
                   <Form.Label>Duration</Form.Label>
-                  <Form.Control type="number" placeholder="weeks" />
+                  <Form.Control
+                    type="number"
+                    placeholder="weeks"
+                    name={durationId}
+                    value={this.state.courseList[idx][durationId]}
+                    onChange={this.handleChange}
+                  />
                 </Form.Group>
               </Form.Row>
             );
           })}
 
           <Form.Row>
-            {/* <fieldset>
-              <Form.Group as={Row}>
-                <Form.Label as="legend" column sm={2}>
-                  Visa
-                </Form.Label>
-                <Col sm={10}>
-                  <Form.Check
-                    type="radio"
-                    label="onshore"
-                    name="formHorizontalRadios"
-                    id="formHorizontalRadios1"
-                  />
-                  <Form.Check
-                    type="radio"
-                    label="offshore"
-                    name="formHorizontalRadios"
-                    id="formHorizontalRadios2"
-                  />
-                </Col>
-              </Form.Group>
-            </fieldset> */}
-
             <Form.Group as={Col} controlId="formGridEnroll">
               <Form.Label>Enrollment Fee</Form.Label>
-              <Form.Control type="text" placeholder="$" />
+              <Form.Control
+                type="text"
+                placeholder="$"
+                name="enrolfee"
+                value={this.state.enrolfee}
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridMaterial">
               <Form.Label>Material Fee</Form.Label>
-              <Form.Control type="text" placeholder="$" />
+              <Form.Control
+                type="text"
+                placeholder="$"
+                name="materalfee"
+                value={this.state.materialfee}
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
